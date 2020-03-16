556d15c376bd6031c01b46cea281b04ebe0f86c4
Caleb Brochu <caleb.brochu@gmail.com>
2020-03-15 17:13:25 -0700

    updating connections.js to push to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,29 @@
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database:"burgers_db"
 })
+}
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database:"burgers_db"
+// })
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log(`Connected as id ${connection.threadId}`)
-});
+// connection.connect(function(err){
+//     if (err) throw err;
+//     console.log(`Connected as id ${connection.threadId}`)
+// });
 
+connection.connect();
 module.exports = connection;
